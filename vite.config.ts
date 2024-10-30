ea2da3881f5cc45e1c97f28ff2760c46fd9b824f
Inti Ortega <intiortega1028@gmail.com>
2024-10-30 15:55:35 -0500

    adding netlify files;

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
 
 declare module "@remix-run/node" {
 	interface Future {
@@ -20,6 +21,7 @@ export default defineConfig({
 				v3_lazyRouteDiscovery: true,
 			},
 		}),
+		netlifyPlugin(),
 		tsconfigPaths(),
 	],
 	ssr: {
